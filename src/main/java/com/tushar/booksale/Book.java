c7e6379c63252b9a7fa5258ab2689449e6e80093
Tushar Goel <tushargoel86@gmail.com>
2022-09-03 16:24:39 +0530

    2nd test case for single type of book

---
+++ src/main/java/com/tushar/booksale/Book.java
@@ -0,0 +1,9 @@
+package com.tushar.booksale;
+
+public class Book {
+    private String name;
+
+    public Book(String name) {
+        this.name = name;
+    }
+}
--- src/main/java/com/tushar/booksale/Cart.java
+++ src/main/java/com/tushar/booksale/Cart.java
@@ -1,11 +1,26 @@
 package com.tushar.booksale;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Cart {
-    public void addBook(BookTitle book, int quantity) {
+
+    public static final double COST_PER_BOOK = 8.0;
+    private Map<Book, Integer> bookQuantityMapping;
+
+    public Cart() {
+        this.bookQuantityMapping = new HashMap<>();
+    }
+
+    public void addBook(Book book, int quantity) {
+        this.bookQuantityMapping.put(book, quantity +
+                    this.bookQuantityMapping.getOrDefault(book, 0));
     }
 
     public double totalValue() {
-        return 8.0;
+        return COST_PER_BOOK * this.bookQuantityMapping.values().stream()
+                            .reduce(Integer::sum)
+                            .orElse(0);
     }
 
 }
