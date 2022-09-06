cd56e8658aed0ed2b15d27137dccc0278e2caa5b
Tushar Goel <tushargoel86@gmail.com>
2022-09-06 21:32:37 +0530

    In this most of the test cases we identified earlier are written and passed. Also, I have not used similar test cases like what happened when n copies of 2,3,4,5 provided. I used only for book1 and thought test cases will not add any value. Similar to other combinations as well.
    In next commit, I will do refactoring related to this.

--- src/test/java/com/tushar/booksale/CartTest.java
+++ src/test/java/com/tushar/booksale/CartTest.java
@@ -17,7 +17,9 @@ public class CartTest {
         List<DiscountStrategy> discountStrategyList =
                 List.of(new NoDiscountStrategy(),
                         new FivePercentageDiscountStrategy(),
-                        new TenPercentageDiscountStrategy());
+                        new TenPercentageDiscountStrategy(),
+                        new TwentyPercentageDiscountStrategy(),
+                        new TwentyFivecentageDiscountStrategy());
         this.cart = new Cart(discountStrategyList);
     }
 
@@ -62,7 +64,7 @@ public class CartTest {
 
     @Test
     @DisplayName("Should get discount of 5 percent for 3 copies of 1st book and 2 copies of 2nd book")
-    public void testForTwoBooks2() {
+    public void testForTwoDifferentBooks2() {
         Book book = new Book("Title-1", 8);
         Book book2 = new Book("Title-2", 8);
         cart.addBook(book);
@@ -79,7 +81,7 @@ public class CartTest {
     @Test
     @DisplayName("Should get discount of 10 percent for 3 copies of 1st book, 2 copies of 2nd book" +
             " and 1 copy of 3rd book")
-    public void testForThreeBooks() {
+    public void testForThreeDifferentBooks() {
         Book book = new Book("Title-1", 8);
         Book book2 = new Book("Title-2", 8);
         Book book3 = new Book("Title-3", 8);
@@ -93,5 +95,49 @@ public class CartTest {
         double price = cart.totalValue();
 
         assertEquals(3 * 8.0 * .9 + 2 * 8.0 * .95 + 8.0, price);
+    }
+
+    @Test
+    @DisplayName("Should get discount of 20% for 3 copies of first book, 2 copies" +
+            " of second book, 2 copies of 3rd book and 1 copy of fourth book")
+    public void testForFourDifferentBooks() {
+        Book book = new Book("Title-1", 8);
+        Book book2 = new Book("Title-2", 8);
+        Book book3 = new Book("Title-3", 8);
+        Book book4 = new Book("Title-4", 8);
+        cart.addBook(book);
+        cart.addBook(book);
+        cart.addBook(book2);
+        cart.addBook(book3);
+        cart.addBook(book2);
+        cart.addBook(book3);
+        cart.addBook(book);
+        cart.addBook(book4);
+
+        double price = cart.totalValue();
+
+        assertEquals(4 * 8.0 * .8 + 3 * 8.0 * .9 + 8.0, price);
+    }
+    @Test
+    @DisplayName("Should get 25% and 10% discount for 3 copies of 1st book," +
+            "2 copies of 2nd book and 1 copy of 3rd book")
+    public void testForFiveDifferentBooks() {
+        Book book = new Book("Title-1", 8);
+        Book book2 = new Book("Title-2", 8);
+        Book book3 = new Book("Title-3", 8);
+        Book book4 = new Book("Title-4", 8);
+        Book book5 = new Book("Title-5", 8);
+        cart.addBook(book);
+        cart.addBook(book2);
+        cart.addBook(book3);
+        cart.addBook(book2);
+        cart.addBook(book3);
+        cart.addBook(book);
+        cart.addBook(book4);
+        cart.addBook(book5);
+
+        double price = cart.totalValue();
+
+        assertEquals(5 * 8.0 * .75 + 3 * 8.0 * .9, price);
     }
 }
